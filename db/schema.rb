c9bc3dcf732e013b501e8907b79e34f6b1b04e5a
aitkhsh <aitkhsh7@yahoo.co.jp>
2024-05-30 16:37:30 +0900

    middle-user-microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_24_052652) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_30_013848) do
   create_table "foods", force: :cascade do |t|
     t.string "genre"
     t.string "mood"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_24_052652) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
